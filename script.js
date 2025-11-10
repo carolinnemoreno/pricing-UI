dd51ba726b123efe50b1ecd550fed072309c5ce6
Carolinne <108836541+carolinnemoreno@users.noreply.github.com>
2025-11-10 12:03:20 -0300

    Add script for monthly and yearly pricing toggle

---
+++ script.js
@@ -0,0 +1,24 @@
+<script>
+const monthly = document.getElementById("monthly");
+const yearly = document.getElementById("yearly");
+
+const precoP = document.querySelector("#precoP");
+const precoB = document.querySelector("#precoB");
+
+monthly.addEventListener("change", () => {
+  if (monthly.checked) {
+    precoP.textContent = "$15";
+	precoB.textContent = "$7";
+  }
+});
+
+yearly.addEventListener("change", () => {
+  if (yearly.checked) {
+    precoP.textContent = "$99";
+	precoB.textContent = "$79";
+  }
+});
+
+
+
+</script>
